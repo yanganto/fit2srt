875f8f6ff0e7947283c95d0c0268970105e6b4e7
Antonio Yang <yanganto@gmail.com>
2025-05-02 21:44:39 +0800

    support time format without ':'

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![allow(special_module_name)]
 use clap::Parser;
+use std::num::ParseIntError;
 use std::path::PathBuf;
 
 mod lib;
@@ -19,22 +20,39 @@ struct Cli {
     fit_file: PathBuf,
 }
 
+fn time_to_vec(time_str: &str) -> Result<Vec<u32>, ParseIntError> {
+    if time_str.contains(':') {
+        let mut err = None;
+        let time = time_str
+            .split(":")
+            .map(|s| match s.parse() {
+                Ok(i) => i,
+                Err(e) => {
+                    err = Some(e);
+                    0
+                }
+            })
+            .collect();
+        if let Some(e) = err {
+            Err(e)
+        } else {
+            Ok(time)
+        }
+    } else {
+        Ok(vec![
+            time_str[0..2].parse()?,
+            time_str[2..4].parse()?,
+            time_str[4..6].parse()?,
+        ])
+    }
+}
+
 fn run(cli: Cli) -> Result<(), Box<dyn std::error::Error + Sync + Send + 'static>> {
     let mut generator = SrtGenerator::default();
 
     if let Some(after_str) = cli.after {
         let mut valid = true;
-        let after_time: Vec<u32> = after_str
-            .split(":")
-            .map(|s| {
-                if let Ok(i) = s.parse() {
-                    i
-                } else {
-                    valid = false;
-                    0
-                }
-            })
-            .collect();
+        let after_time: Vec<u32> = time_to_vec(&after_str)?;
 
         if after_time.len() == 3 && after_time[0] <= 24 && after_time[1] < 60 || after_time[2] < 60
         {
@@ -54,17 +72,7 @@ fn run(cli: Cli) -> Result<(), Box<dyn std::error::Error + Sync + Send + 'static
 
     if let Some(before_str) = cli.before {
         let mut valid = true;
-        let before_time: Vec<u32> = before_str
-            .split(":")
-            .map(|s| {
-                if let Ok(i) = s.parse() {
-                    i
-                } else {
-                    valid = false;
-                    0
-                }
-            })
-            .collect();
+        let before_time: Vec<u32> = time_to_vec(&before_str)?;
 
         if before_time.len() == 3 && before_time[0] <= 24 && before_time[1] < 60
             || before_time[2] < 60
@@ -93,4 +101,10 @@ fn main() {
     if let Err(e) = run(Cli::parse()) {
         println!("{e:?}");
     }
+}
+
+#[test]
+fn test_time_to_vec() {
+    assert_eq!(time_to_vec("15:01:30"), Ok(vec![15, 1, 30]));
+    assert_eq!(time_to_vec("150130"), Ok(vec![15, 1, 30]));
 }
